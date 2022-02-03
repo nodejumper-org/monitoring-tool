befe9f90cb12002a1d7377b90ce6bd37ac4f9285
Valentin Bloher <vbloher7@gmail.com>
2022-02-03 22:56:06 +0200

    add install exporter script

---
+++ utils/install_node_exporter.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install -y wget
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+tar xvf node_exporter-1.3.1.linux-amd64.tar.gz
+rm node_exporter-1.3.1.linux-amd64.tar.gz
+sudo mv node_exporter-1.3.1.linux-amd64 node_exporter
+chmod +x $HOME/node_exporter/node_exporter
+sudo mv $HOME/node_exporter/node_exporter /usr/bin
+rm -Rvf $HOME/node_exporter/
+
+sudo tee /etc/systemd/system/exporterd.service > /dev/null <<EOF
+[Unit]
+Description=node_exporter
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=/usr/bin/node_exporter
+Restart=always
+RestartSec=3
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable exporterd
+sudo systemctl start exporterd
