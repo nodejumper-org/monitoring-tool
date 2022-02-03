869f8b746c15a93a301e427fed7557dedbc427dd
Valentin Bloher <vbloher7@gmail.com>
2022-02-03 15:08:21 +0200

    install docker-compose too

--- utils/install_docker.sh
+++ utils/install_docker.sh
@@ -10,3 +10,7 @@ echo \
 
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
