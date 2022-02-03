4fa1bb49afefb341dac304f330455f39c14bf958
Valentin Bloher <vbloher7@gmail.com>
2022-02-03 15:01:05 +0200

    use docker’s repositories

--- utils/install_docker.sh
+++ utils/install_docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 sudo apt update
-sudo apt install wget -y
-wget -O get-docker.sh https://get.docker.com
-sudo sh get-docker.sh
-sudo apt install -y docker-compose
-rm -f get-docker.sh
-sudo usermod -aG docker $USER
+sudo apt install -y ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
