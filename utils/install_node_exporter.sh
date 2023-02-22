4c4d1ac339e215f731324706d6e717495b7aa5a1
Valentin Bloher <vbloher7@gmail.com>
2023-02-22 19:46:43 +0700

    update node_exporter version

--- utils/install_node_exporter.sh
+++ utils/install_node_exporter.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt update && sudo apt install -y wget
-wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
-tar xvf node_exporter-1.3.1.linux-amd64.tar.gz
-rm node_exporter-1.3.1.linux-amd64.tar.gz
-sudo mv node_exporter-1.3.1.linux-amd64 node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+tar xvf node_exporter-1.5.0.linux-amd64.tar.gz
+rm node_exporter-1.5.0.linux-amd64.tar.gz
+sudo mv node_exporter-1.5.0.linux-amd64 node_exporter
 chmod +x $HOME/node_exporter/node_exporter
 sudo mv $HOME/node_exporter/node_exporter /usr/bin
 rm -Rvf $HOME/node_exporter/
