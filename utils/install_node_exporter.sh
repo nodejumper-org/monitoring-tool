50982ad9bbadcd6af0586f44dc21b955e2beef62
vbloher <vbloher7@gmail.com>
2023-09-29 20:28:58 +0700

    update node_exporter installer

--- utils/install_node_exporter.sh
+++ utils/install_node_exporter.sh
@@ -1,28 +1,60 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y wget
-wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
-tar xvf node_exporter-1.5.0.linux-amd64.tar.gz
-rm node_exporter-1.5.0.linux-amd64.tar.gz
-sudo mv node_exporter-1.5.0.linux-amd64 node_exporter
-chmod +x $HOME/node_exporter/node_exporter
-sudo mv $HOME/node_exporter/node_exporter /usr/bin
-rm -Rvf $HOME/node_exporter/
+# Check the architecture
+if [[ "$(uname -m)" == "x86_64" ]]; then
+  ARCH="amd64"
+elif [[ "$(uname -m)" == "armv7l" ]]; then
+  ARCH="arm"
+else
+  echo "Unsupported architecture"
+  exit 1
+fi
 
-sudo tee /etc/systemd/system/exporterd.service > /dev/null <<EOF
+# Determine the operating system
+if [[ "$(uname -s)" == "Linux" ]]; then
+  OS="linux"
+elif [[ "$(uname -s)" == "Darwin" ]]; then
+  OS="darwin"
+else
+  echo "Unsupported operating system"
+  exit 1
+fi
+
+VERSION=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep tag_name | cut -d '"' -f 4)
+URL="https://github.com/prometheus/node_exporter/releases/latest/download/node_exporter-$VERSION.$OS-$ARCH.tar.gz"
+
+# Create a temporary directory for downloading and installation
+TMP_DIR=$(mktemp -d)
+cd "$TMP_DIR"
+
+# Download and unpack node_exporter
+curl -LO "$URL"
+tar xzf "node_exporter-$VERSION.$OS-$ARCH.tar.gz"
+sudo cp "node_exporter-$VERSION.$OS-$ARCH/node_exporter" /usr/local/bin/
+
+# Create a systemd unit file
+cat <<EOF | sudo tee /etc/systemd/system/node_exporter.service
 [Unit]
 Description=node_exporter
 After=network-online.target
+
 [Service]
 User=$USER
-ExecStart=/usr/bin/node_exporter
+ExecStart=/usr/local/bin/node_exporter
 Restart=always
 RestartSec=3
 LimitNOFILE=65535
+
 [Install]
 WantedBy=multi-user.target
 EOF
 
+# Reload systemd and start the service
 sudo systemctl daemon-reload
-sudo systemctl enable exporterd
-sudo systemctl start exporterd
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
+
+# Clean up the temporary directory
+rm -rf "$TMP_DIR"
+
+echo "Node Exporter version $VERSION successfully installed and started."
