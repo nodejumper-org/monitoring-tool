9f59d384ef8e7b923232b38c6079aab8b6ed7e62
vbloher <vbloher7@gmail.com>
2024-03-29 23:20:02 +0700

    update scripts

--- utils/install_docker.sh
+++ utils/install_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo apt -q update
+sudo apt -qy install ca-certificates curl gnupg lsb-release
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -10,7 +10,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt -q update
+sudo apt -qy install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo usermod -aG docker $USER
--- utils/install_node_exporter.sh
+++ utils/install_node_exporter.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y wget
-wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
-tar xvf node_exporter-1.5.0.linux-amd64.tar.gz
-rm node_exporter-1.5.0.linux-amd64.tar.gz
-sudo mv node_exporter-1.5.0.linux-amd64 node_exporter
+# Install the necessary packages
+sudo apt -q update
+sudo apt -qy install curl wget
+
+# Define the latest version of node_exporter
+VERSION=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+# Determine the operating system
+if [[ "$(uname -s)" == "Linux" ]]; then
+  OS="linux"
+elif [[ "$(uname -s)" == "Darwin" ]]; then
+  OS="darwin"
+else
+  echo "Unsupported operating system"
+  exit 1
+fi
+
+# Determine the architecture
+if [[ "$(uname -m)" == "x86_64" ]]; then
+  ARCH="amd64"
+elif [[ "$(uname -m)" == "aarch64" ]]; then
+  ARCH="arm64"
+else
+  echo "Unsupported architecture"
+  exit 1
+fi
+
+# Download and install node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/${VERSION}/node_exporter-${VERSION}.${OS}-${ARCH}.tar.gz
+tar xvf node_exporter-${VERSION}.${OS}-${ARCH}.tar.gz
+rm node_exporter-${VERSION}.${OS}-${ARCH}.tar.gz
+sudo mv node_exporter-${VERSION}.${OS}-${ARCH} node_exporter
 chmod +x $HOME/node_exporter/node_exporter
 sudo mv $HOME/node_exporter/node_exporter /usr/bin
 rm -Rvf $HOME/node_exporter/
 
+# Create a systemd service for node_exporter
 sudo tee /etc/systemd/system/exporterd.service > /dev/null <<EOF
 [Unit]
 Description=node_exporter
@@ -23,6 +51,7 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
+# Enable and start the node_exporter service
 sudo systemctl daemon-reload
 sudo systemctl enable exporterd
 sudo systemctl start exporterd
